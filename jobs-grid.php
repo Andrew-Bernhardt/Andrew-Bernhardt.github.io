8c493c95a8a580759ce86a61be3096bcf3c18a21
Andrew Bernhardt <aeb30@email.sc.edu>
2023-08-17 16:02:31 -0400

    cool usc effect

--- jobs-grid.php
+++ jobs-grid.php
@@ -9,12 +9,11 @@
 
     <div class="jobs-grid">
         <div class="job-grid-item job-grid-expand-on-hover grid-job-title"><h1>Past Work</h1></div>
-        <div class="job-grid-item job-grid-expand grid-job-terminix">
+        <div class="job-grid-item grid-job-terminix">
             <div class="job-info">
                 <img src="./images/terminix-logo.png"/>
                 <h3>Job Title: Web Developer</h3>
             </div>
-            
         </div>
         <div class="job-grid-item grid-job-capgemini">
             <img src= "./images/Capgemini-Logo.png"/>
@@ -22,8 +21,16 @@
         <div class="job-grid-item grid-job-softdocs">
             <img src="./images/softdocs.png"/>
         </div>
-        <div class="job-grid-item job-grid-tutoring">
-            <h3>Tutoring</h3>
+        <div class="job-prac">
+            <div class="blur-box">
+                <img src="./images/usc_logo.png" alt="">
+                <p>Hello</p>
+                <p>Peer</p>
+                <p>Tutor</p>
+            </div>
+        </div>
+        <div class="job-grid-item degen">
+            kjadfkjk
         </div>
         
     </div>
